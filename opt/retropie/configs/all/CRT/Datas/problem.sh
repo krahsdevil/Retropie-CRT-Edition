179ae03b9f0ab270009c17440e99490e1437b340
krahsdevil <dvilches@msn.com>
2019-04-10 16:01:47 +0200

    Clear _assets branch

--- opt/retropie/configs/all/CRT/Datas/problem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "1" > /tmp/problem.srt
-echo "00:00:00,000 --> 00:00:07,000" >> /tmp/problem.srt
-echo $1 >> /tmp/problem.srt
-echo $2 >> /tmp/problem.srt
-omx_fnt="--font=/usr/share/fonts/dejavu/DejaVuSans-BoldOblique.ttf"
-omx_opt="--no-keys --layer=10000 --aspect-mode=fill"
-omx_srt="--no-ghost-box --lines=2 --align=center $omx_fnt --font-size=50 --subtitles=/tmp/problem.srt"
-/usr/bin/omxplayer.bin $omx_srt /opt/retropie/configs/all/CRT/Datas/problem.mp4 > /dev/null
--- opt/retropie/configs/all/runcommand-onstart.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo python /opt/retropie/configs/all/CRT/bin/GeneralModule/ES_Systems_Check.py
