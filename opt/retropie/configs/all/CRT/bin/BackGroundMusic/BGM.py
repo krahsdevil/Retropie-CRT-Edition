bed8b384c43766f2c935804783a4af0dfc84af73
krahsdevil <dvilches@msn.com>
2019-05-27 21:39:08 +0200

    Background Music Fix
    Sometimes Black Screen on Exit

--- opt/retropie/configs/all/CRT/bin/BackGroundMusic/BGM.py
+++ opt/retropie/configs/all/CRT/bin/BackGroundMusic/BGM.py
@@ -277,7 +277,7 @@ while True:
                 #print "Emulator finished, resuming audio..."
                 #Wait to CRT Launcher to exit
                 while True:
-                    time.sleep(1)
+                    time.sleep(1.8)
                     output = commands.getoutput('ps -fe')
                     if not CRTProcess in output:
                         break
