8a90bf220a4d6d0a4cc8d4dda8dcecb778de3116
krahsdevil <dvilches@msn.com>
2019-06-18 19:44:02 +0200

    Background Music Fix v2

--- opt/retropie/configs/all/CRT/bin/BackGroundMusic/BGM.py
+++ opt/retropie/configs/all/CRT/bin/BackGroundMusic/BGM.py
@@ -277,7 +277,7 @@ while True:
                 #print "Emulator finished, resuming audio..."
                 #Wait to CRT Launcher to exit
                 while True:
-                    time.sleep(1.8)
+                    time.sleep(2.3)
                     output = commands.getoutput('ps -fe')
                     if not CRTProcess in output:
                         break
