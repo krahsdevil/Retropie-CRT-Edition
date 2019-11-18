ecd1d64749ae1347b6965e8a7fde9530a4cfef75
krahsdevil <dvilches@msn.com>
2019-11-18 18:33:51 +0100

    Update videoplayer.py

--- opt/retropie/configs/all/CRT/bin/GeneralModule/launcher_module/plugins/videoplayer.py
+++ opt/retropie/configs/all/CRT/bin/GeneralModule/launcher_module/plugins/videoplayer.py
@@ -3,7 +3,7 @@
 
 
 """
-launcher videoplayer.py.
+videoplayer.py
 
 launcher library for retropie, based on original idea - Ironic
   and the retropie integration work by -krahs-
@@ -28,53 +28,59 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os, re, sys, logging, commands, glob, subprocess
 from launcher_module.utils import splash_info
 from launcher_module.core_choices_dynamic import choices
-from launcher_module.core import launcher, CRTROOT_PATH, RETROPIEEMU_PATH, RETROPIECFG_PATH, CFG_TIMINGS_FILE
+from launcher_module.core import launcher, CFG_TIMINGS_FILE
+from launcher_module.core_paths import *
 from launcher_module.screen import CRT
 
-OMXPLAYER_FONT = '--font="/opt/retropie/configs/all/CRT/bin/ScreenUtilityFiles/resources/assets/screen_videoplayer/Ubuntu_MOD_WIDE.ttf'
-OMXPLAYER_COMMAND = 'omxplayer -b --align center --layer 10000 --font-size 72 %s" \"%s\" > /dev/null 2>&1' % (OMXPLAYER_FONT, "%s")
-JOY2KEY_DEV = "none"
-JOY2KEY_PATH = "/opt/retropie/configs/all/CRT/bin/ScreenUtilityFiles/resources/assets/screen_videoplayer/joy2key.py"
-JOY2KEY_VAR = commands.getoutput('$__joy2key_dev')
+CRTASSETS_VIDEO_PATH = os.path.join(CRTASSETS_PATH, "screen_videoplayer")
+FONT_FILE = os.path.join(CRTASSETS_VIDEO_PATH, "Ubuntu_MOD_WIDE.ttf")
+JOY2KEY_FILE = os.path.join(CRTASSETS_VIDEO_PATH, "joy2key.py")
 
 class videoplayer(launcher):
-    m_sSystemCfg = ""
-    m_sSystemCfgPath = ""
-    m_sVideoPath = ""
-    m_sMultipleVideos = False
-    m_nVideoPosition = 0
-    m_nVideoFoundNumber = 0
+    m_nVideoPOS = 0
+    m_nVideoNUM = 0
     m_lVideoEXT = ['avi', 'mkv', 'mp4', 'mpg', 'AVI', 'MKV', 'MP4', 'MPG']
-    m_lVideoList = []
+    m_lVideoLST = []
+
+    m_sPlayAll = "False"
+
+    """ OMX Player command string """
+    m_sOMXPart01 = 'omxplayer -b --align center --layer 10000 '
+    m_sOMXPart02 = '--font-size 72 --font="%s" ' % (FONT_FILE)
+    m_sOMXPart03 = '"%s" > /dev/null 2>&1' % ("%s")
+    m_sOMXPCommand = m_sOMXPart01 + m_sOMXPart02 + m_sOMXPart03
 
     @staticmethod
     def get_system_list():
         return ["videoplayer"]
 
     def pre_configure(self):
-        if not os.path.isfile(JOY2KEY_PATH):
-            logging.info("closing videoplayer: can't find file: %s" % JOY2KEY_PATH)
+        if not os.path.isfile(JOY2KEY_FILE):
+            logging.info("closing videoplayer: can't find file: %s" % \
+                         JOY2KEY_FILE)
             self.panic("can't find joy2key", "try again!")
-    # system configure vars
+
     def configure(self):
         if self.m_sSystem == "videoplayer":
             self.m_sSystemFreq = "videoplayer50"
 
-     # just called if need rebuild the CMD
     def prepare(self):
         #if more than one video and selected is not the last
-        if self.find_videos() and (self.m_nVideoFoundNumber != self.m_nVideoPosition):
-            self.m_sMultipleVideos = self.video_options()
+        if self._find_videos() and \
+           (self.m_nVideoNUM != self.m_nVideoPOS+1):
+            self.m_sPlayAll = self.video_options()
 
     def runcommand_start(self):
         """ launch_videoplayer!"""
-        commandline = OMXPLAYER_COMMAND % self.m_lVideoList[self.m_nVideoPosition]
-        
-        logging.info("Launching Joy2Key")  
-        self.launch_joy2key('kcub1', 'kcuf1', 'kcuu1', 'kcud1', '0x20', '0x71', '0x6b', '0x6a', '0x6d', '0x6e')
-        logging.info("Playing video: %s", self.m_lVideoList[self.m_nVideoPosition])        
+        commandline = self.m_sOMXPCommand % \
+                      self.m_lVideoLST[self.m_nVideoPOS]
+        logging.info("Launching Joy2Key")
+        self._launch_joy2key('kcub1', 'kcuf1', 'kcuu1', 'kcud1', '0x20', '0x71',
+                            '0x6b', '0x6a', '0x6d', '0x6e')
+        logging.info("Playing video: %s" % \
+                     self.m_lVideoLST[self.m_nVideoPOS])
         self.m_oRunProcess = subprocess.Popen(commandline, shell=True)
-        logging.info("Subprocess running: %s", commandline)
+        logging.info("Subprocess running: %s" % commandline)
 
     def screen_set(self):
         self.m_oCRT = CRT(self.m_sSystemFreq)
@@ -93,12 +99,14 @@ class videoplayer(launcher):
                 if returncoded == 3:
                     break
                 else:
-                    if self.m_sMultipleVideos == "True":
-                        self.m_nVideoPosition += 1
+                    if self.m_sPlayAll == "True":
+                        self.m_nVideoPOS += 1
                         try:
-                            self.m_lVideoList[self.m_nVideoPosition]
-                            logging.info("Playing video: %s", self.m_lVideoList[self.m_nVideoPosition])
-                            commandline = OMXPLAYER_COMMAND % self.m_lVideoList[self.m_nVideoPosition]
+                            self.m_lVideoLST[self.m_nVideoPOS]
+                            logging.info("Playing video: %s" % \
+                                         self.m_lVideoLST[self.m_nVideoPOS])
+                            commandline = self.m_sOMXPCommand % \
+                                          self.m_lVideoLST[self.m_nVideoPOS]
                             self.m_oRunProcess = subprocess.Popen(commandline, shell=True)
                             logging.info("Subprocess running: %s", commandline)
                         except:
@@ -106,7 +114,7 @@ class videoplayer(launcher):
                     else:
                         break
         logging.info("process end")
-        
+
     def cleanup(self):
         #os.system('echo %s >> /tmp/proces' % returncoded)
         os.system('sudo killall joy2key.py')
@@ -115,44 +123,41 @@ class videoplayer(launcher):
         os.system('clear')
         sys.exit()
 
-    def launch_joy2key(self, left, right, up, down, a, b, x, y, start, select):
+    def _launch_joy2key(self, left, right, up, down, a, b, x, y, start, select):
         # get the first joystick device (if not already set)
+        JOY2KEY_VAR = commands.getoutput('$__joy2key_dev')
+        JOY2KEY_DEV = "/dev/input/jsX"
         if os.path.exists (JOY2KEY_VAR):
             JOY2KEY_DEV = JOY2KEY_VAR
-        else:
-            JOY2KEY_DEV = "/dev/input/jsX"
         output = commands.getoutput('ps -A')
-        if os.path.exists (JOY2KEY_PATH) and JOY2KEY_DEV != "none" and not 'joy2key.py' in output:
-            joy2key_command = "\"%s\" \"%s\" %s %s %s %s %s %s %s %s %s %s" % (JOY2KEY_PATH,JOY2KEY_DEV,left, right,up,down,a,b,x,y,start,select)
-            process = subprocess.Popen(joy2key_command, shell=True)
+        if os.path.exists (JOY2KEY_FILE) and JOY2KEY_DEV != "none" and \
+           not 'joy2key.py' in output:
+            p_sJ2KCommand = '"%s" "%s" %s %s %s %s %s %s %s %s %s %s' % \
+                               (JOY2KEY_FILE, JOY2KEY_DEV, left, right, up, \
+                               down, a, b, x, y, start, select)
+            process = subprocess.Popen(p_sJ2KCommand, shell=True)
             return process
 
-    def find_videos(self):
+    def _find_videos(self):
         self.m_sFileDir
         for extension in self.m_lVideoEXT:
-            self.m_lVideoList = self.m_lVideoList + glob.glob(("%s/*.%s")%(self.m_sFileDir,extension))
-        self.m_lVideoList = sorted(self.m_lVideoList)
-        
-        counter = 0
-        for video in self.m_lVideoList:
-            if video == self.m_sFilePath:
-                self.m_nVideoPosition = counter
-            counter += 1
-        self.m_nVideoFoundNumber = counter-1
-        logging.info("Detected %s videos in the same folder", counter)
-        if self.m_nVideoFoundNumber >= 1:
+            self.m_lVideoLST = self.m_lVideoLST + glob.glob(("%s/*.%s") % \
+                                                    (self.m_sFileDir,extension))
+        self.m_lVideoLST = sorted(self.m_lVideoLST)
+        self.m_nVideoNUM = len(self.m_lVideoLST)
+        self.m_nVideoPOS = self.m_lVideoLST.index(self.m_sFilePath)
+        logging.info("Detected %s videos, selected in position number %s" % \
+                    (self.m_nVideoNUM, self.m_nVideoPOS))
+        if self.m_nVideoNUM >= 1:
             return True
-        else:
-            return False
+        return False
 
     def video_options(self):
         ch = choices()
-        ch.set_title("MULTIPLE VIDEOS FOUND")
+        ch.set_title("VIDEO PLAYER")
         ch.load_choices([
-                ("Play only this video.", "False"),
+                ("Just this video...", "False"),
                 ("Play ALL from this!", "True"),
             ])
         result = ch.run()
         return result
-
-        
