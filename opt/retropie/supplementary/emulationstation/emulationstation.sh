5ce730f90e9e6dab16bb467b1ef1950217ff5a2c
krahsdevil <34064192+krahsdevil@users.noreply.github.com>
2020-01-24 10:42:29 +0100

    'first-boot' control in main retropie emulationstation script

--- opt/retropie/supplementary/emulationstation/emulationstation.sh
+++ opt/retropie/supplementary/emulationstation/emulationstation.sh
@@ -2,9 +2,12 @@
 ang=0
 esdir="$(dirname $0)"
 astdir="/opt/retropie/configs/all/CRT/bin/ScreenUtilityFiles/resources/assets/screen_emulationstation/CRTResources/configs"
+cablesel="/opt/retropie/configs/all/CRT/bin/ScreenUtilityFiles/bin/module_rgb_cable_switcher/CRT-RGB-Cable_Launcher.py"
 tate1="$astdir/es-select-tate1"
 tate3="$astdir/es-select-tate3"
 yoko="$astdir/es-select-yoko"
+fstboot="$astdir/first-boot"
+
 if [ -f $tate1 ]; then
     ang=1
 elif [ -f $tate3 ]; then
@@ -17,7 +20,12 @@ fi
 
 while true; do
     rm -f /tmp/es-restart /tmp/es-sysrestart /tmp/es-shutdown
-    "$esdir/emulationstation" --screenrotate $ang "$@"
+    if [ -f $fstboot ]; then
+		rm -f $fstboot
+		python $cablesel
+	else
+		"$esdir/emulationstation" --screenrotate $ang "$@"
+	fi
     ret=$?
     if [ -f /tmp/es-restart ]; then
         if [ -f $tate1 ]; then
