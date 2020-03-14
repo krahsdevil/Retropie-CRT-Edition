a527892b7891633e22040573fb58d6810fdd54be
krahsdevil <34064192+krahsdevil@users.noreply.github.com>
2020-03-14 20:51:09 +0100

    Update file_helpers.py

--- opt/retropie/configs/all/CRT/bin/GeneralModule/launcher_module/file_helpers.py
+++ opt/retropie/configs/all/CRT/bin/GeneralModule/launcher_module/file_helpers.py
@@ -7,7 +7,7 @@ file_helpers.py.
 
 https://github.com/krahsdevil/crt-for-retropie/
 
-Copyright (C)  2018/2019 -krahs- - https://github.com/krahsdevil/
+Copyright (C)  2018/2020 -krahs- - https://github.com/krahsdevil/
 Copyright (C)  2019 dskywalk - http://david.dantoine.org
 
 This program is free software: you can redistribute it and/or modify it under
