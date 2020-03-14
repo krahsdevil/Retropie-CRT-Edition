e93666a6e359c9e588767df31490da2635fdedae
krahsdevil <34064192+krahsdevil@users.noreply.github.com>
2020-03-14 20:54:00 +0100

    Update pattern_datas.py

--- opt/retropie/configs/all/CRT/bin/ScreenUtilityFiles/bin/module_screen_center_utility/pattern_datas.py
+++ opt/retropie/configs/all/CRT/bin/ScreenUtilityFiles/bin/module_screen_center_utility/pattern_datas.py
@@ -8,7 +8,7 @@ Centering Pattern Utility for CRT image adjusting by Krahs
 
 https://github.com/krahsdevil/crt-for-retropie/
 
-Copyright (C)  2018/2019 -krahs- - https://github.com/krahsdevil/
+Copyright (C)  2018/2020 -krahs- - https://github.com/krahsdevil/
 Copyright (C)  2019 dskywalk - http://david.dantoine.org
 
 This program is free software: you can redistribute it and/or modify it under
