ba17b7d3683de6e1dd1c8530271d3d04f2e75fbc
krahsdevil <34064192+krahsdevil@users.noreply.github.com>
2020-03-14 20:48:37 +0100

    Update emulator_launcher.py

--- opt/retropie/configs/all/CRT/bin/GeneralModule/emulator_launcher.py
+++ opt/retropie/configs/all/CRT/bin/GeneralModule/emulator_launcher.py
@@ -10,7 +10,7 @@ launcher library for retropie, based on original idea - Ironic
 
 https://github.com/krahsdevil/crt-for-retropie/
 
-Copyright (C)  2018/2019 -krahs- - https://github.com/krahsdevil/
+Copyright (C)  2018/2020 -krahs- - https://github.com/krahsdevil/
 Copyright (C)  2019 dskywalk - http://david.dantoine.org
 
 This program is free software: you can redistribute it and/or modify it under
